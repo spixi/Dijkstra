185dfcbff8019686aaf3f6c45efceb669454afa6
serjoscha-87 <serjoscha-87@192.168.1.185>
2013-06-21 14:18:40 +0200

    added some features the gui needs

--- src/model/ListDataModel.java
+++ src/model/ListDataModel.java
@@ -1,9 +1,12 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 public interface ListDataModel {
 
 	public Vector<String> getLocations();
+	public String getFormattedDuration();
+	public ArrayList<String> getRouteHops();
 	
 }
