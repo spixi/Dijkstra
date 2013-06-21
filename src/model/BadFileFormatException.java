6dc7896831e45898aa5c21367eda5a9237739052
Marius <marius.spix@web.de>
2013-06-21 09:50:17 +0200

    created BadFileFormatException

---
+++ src/model/BadFileFormatException.java
@@ -0,0 +1,10 @@
+package model;
+
+public class BadFileFormatException extends Exception {
+
+	private static final long serialVersionUID = 297546714301753024L;
+
+	public BadFileFormatException(String message) {
+		super(message);
+	}
+}
