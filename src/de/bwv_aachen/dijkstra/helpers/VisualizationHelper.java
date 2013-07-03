d7b1c2a33fec4cbd34346f461ee4db1d4e56fe9d
Marius <marius.spix@web.de>
2013-07-03 22:20:17 +0200

    Airports are now drawn!

---
+++ src/de/bwv_aachen/dijkstra/helpers/VisualizationHelper.java
@@ -0,0 +1,12 @@
+package de.bwv_aachen.dijkstra.helpers;
+
+public class VisualizationHelper {
+    
+    double panelWidth, panelHeight;
+    
+    public VisualizationHelper(double panelWidth, double panelHeight) {
+        this.panelWidth  = panelWidth;
+        this.panelHeight = panelHeight;
+    }
+
+}
